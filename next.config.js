ea2098332c305df4418a36b46bdddb41b1e9065e
freeman-lab <the.freeman.lab@gmail.com>
2021-03-14 11:47:07 -0700

    try other base path

--- next.config.js
+++ next.config.js
@@ -2,5 +2,5 @@ const isDev =
   process.env.VERCEL_ENV === 'preview' || process.env.NODE_ENV === 'development'
 
 module.exports = {
-  basePath: isDev ? '' : '/jeremyfreeman.net/blog',
+  assetPrefix: isDev ? '' : 'https://blog.jeremyfreeman.net',
 }
