0fdff7a6646fbfd680a54cc640af5ab859167175
freeman-lab <the.freeman.lab@gmail.com>
2021-03-14 11:57:32 -0700

    add theme

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
-import { Box } from 'theme-ui'
+import { useCallback } from 'react'
+import { useColorMode, Button, Box } from 'theme-ui'
 
 const Index = () => {
-  return <Box sx={{fontSize: [5], px: [4], py: [4]}}>blog</Box>
+  const [colorMode, setColorMode] = useColorMode()
+
+  const toggle = useCallback(() => {
+    setColorMode(colorMode === 'light' ? 'dark' : 'light')
+  }, [colorMode])
+
+  return <Box sx={{px: [4], py: [4]}}>
+    <Box sx={{fontSize: [5]}}>blog</Box>
+    <Button onClick={toggle}>Toggle theme</Button>
+  </Box>
 }
 
 export default Index
